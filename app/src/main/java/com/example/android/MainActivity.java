53b0b17df76b5ced5b0887ffaed45e842429235b
KGallyamov <mirakpjcppr@gmail.com>
2020-01-18 22:05:25 +0300

    CardView и RecyclerView

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -7,87 +7,71 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bnv;
-    ListView list, liked;
     SharedPreferences sPref;
     TextView textView;
-    
+    private RecyclerView recyclerView;
+    private MyAdapter adapter;
+    private List<RecyclerItem> listItems;
     final String SAVED = "favorite_posts";
-    int[] like = {11, 15, 16, 20, 45}; //TODO: ArrayList
     Activity getActivity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView pawn = (ImageView) findViewById(R.id.imageView);
-
-
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageRef = storage.getReference();
-
-
-        list = (ListView) findViewById(R.id.listView);
-        liked = (ListView) findViewById(R.id.liked);
         textView = (TextView) findViewById(R.id.textView2);
         bnv = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        String[] val = new String[50];
-        String[] l = new String[like.length];
-        for(int i=0;i<val.length;++i){
-            val[i] = "post " + (i+1);
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++){
+            RecyclerItem item = new RecyclerItem("Item " + (i + 1), "Description of item " + (i + 1));
+            listItems.add(item);
         }
-        for(int i=0;i<l.length;i++){
-            l[i] = "post " + like[i];
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, val);
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, l);
-        list.setAdapter(adapter);
-        liked.setAdapter(adapter1);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch(position) {
-                    case 11:
-                    case 0:
-                        finish();
-                        //TODO: add to favorites
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
+
+        //Adapter
+        adapter = new MyAdapter(listItems, this);
+        recyclerView.setAdapter(adapter);
+
+
+
         saveFavorite();
         bnv.setOnNavigationItemSelectedListener(getBottomNavigationListener());
     }
-
-
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
         saveFavorite();
     }
-
     void saveFavorite() {
         sPref = getPreferences(MODE_PRIVATE);
         Editor ed = sPref.edit();
         String s = "";
-        for(int i=0;i<like.length;i++){
-            s += Integer.toString(like[i]) + " ";
-        }
+
         ed.putString(SAVED, s);
         ed.apply();
     }
@@ -99,18 +83,14 @@ public class MainActivity extends AppCompatActivity {
                 switch(menuItem.getItemId()){
                     case R.id.action_profile:
                         textView.setVisibility(View.VISIBLE);
-                        list.setVisibility(View.INVISIBLE);
-                        liked.setVisibility(View.INVISIBLE);
+                        recyclerView.setVisibility(View.INVISIBLE);
                         break;
                     case R.id.action_posts:
                         textView.setVisibility(View.INVISIBLE);
-                        list.setVisibility(View.VISIBLE);
-                        liked.setVisibility(View.INVISIBLE);
+                        recyclerView.setVisibility(View.VISIBLE);
                         break;
                     case R.id.action_1:
                         textView.setVisibility(View.INVISIBLE);
-                        list.setVisibility(View.INVISIBLE);
-                        liked.setVisibility(View.VISIBLE);
                 }
                 return true;
             }
---
+++ app/src/main/java/com/example/android/MyAdapter.java
@@ -0,0 +1,93 @@
+package com.example.android;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+    private List<RecyclerItem> listItems;
+    private Context mContext;
+
+    public MyAdapter(List<RecyclerItem> listItems, Context mContext) {
+        this.listItems = listItems;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+        final RecyclerItem itemList = listItems.get(position);
+
+
+        holder.txtTitle.setText(itemList.getTitle());
+        holder.txtDescription.setText(itemList.getDescription());
+        holder.txtOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //нажатие на элемент CardView
+                final PopupMenu popupMenu = new PopupMenu(mContext, holder.txtOption);
+                popupMenu.inflate(R.menu.option_menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.mnu_item_save:
+                                Toast.makeText(mContext, "Saved", Toast.LENGTH_LONG).show();
+                                break;
+                            case R.id.mnu_item_delete:
+                                listItems.remove((position));
+                                notifyDataSetChanged();
+                                Toast.makeText(mContext, "Deleted", Toast.LENGTH_LONG).show();
+                                break;
+                            default:
+                                break;
+                        }
+
+                        return false;
+                    }
+                });
+                popupMenu.show();
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView txtTitle;
+        public TextView txtDescription;
+        public TextView txtOption;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            txtDescription = itemView.findViewById(R.id.txtDescription);
+            txtOption = itemView.findViewById(R.id.txtOptionDigit);
+
+
+        }
+
+    }
+}
