b7d8417019e6192395563f863e548a65c22cca05
KGallyamov <mirakpjcppr@gmail.com>
2020-02-19 15:30:59 +0300

    Adding link to image in storage in database

--- app/src/main/java/com/example/android/Add_post.java
+++ app/src/main/java/com/example/android/Add_post.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,9 +44,11 @@ public class Add_post extends Fragment implements View.OnClickListener {
     Button photo, post;
     private Uri filePath;
     public String txtTitle="Title", txtDescription="Description",
-            txtImage="https://firebasestorage.googleapis.com/v0/b/android-824bc.appspot.com/o/images%2F397f6d9b-31d7-4aec-9fd4-426e5fb3c104?alt=media&token=f7d2a505-9831-4d34-8f15-02069bd4a580";
+            //txtImage="https://firebasestorage.googleapis.com/v0/b/android-824bc.appspot.com/o/images%2F397f6d9b-31d7-4aec-9fd4-426e5fb3c104?alt=media&token=f7d2a505-9831-4d34-8f15-02069bd4a580";
+            txtImage = "0-0";
     FirebaseStorage storage;
     StorageReference storageReference;
+    String link;
 
     private final int PICK_IMAGE_REQUEST = 71;
 
@@ -126,14 +129,13 @@ public class Add_post extends Fragment implements View.OnClickListener {
                 break;
             case R.id.button_send:
                 uploadImage();
-                uploadPost();
             default:
                 break;
         }
     }
 
-    private void uploadPost() {
-        Data data = new Data(txtDescription, txtImage, txtTitle);
+    private void uploadPost(String image) {
+        Data data = new Data(txtDescription, image, txtTitle);
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
         databaseReference.child("Data").push().setValue(data, new DatabaseReference.CompletionListener() {
@@ -144,6 +146,7 @@ public class Add_post extends Fragment implements View.OnClickListener {
                 description.setText("");
             }
         });
+
     }
 
     @Override
@@ -172,14 +175,20 @@ public class Add_post extends Fragment implements View.OnClickListener {
             progressDialog.setTitle("Uploading...");
             progressDialog.show();
 
-            StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
+            final StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
             ref.putFile(filePath)
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                             progressDialog.dismiss();
-                            Toast.makeText(getActivity(), "Uploaded", Toast.LENGTH_SHORT).show();
-                            imageView.setImageResource(android.R.color.transparent);
+                            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String image = uri.toString();
+                                    imageView.setImageResource(android.R.color.transparent);
+                                    uploadPost(image);
+                                }
+                            });
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
--- app/src/main/java/com/example/android/NewsFeed.java
+++ app/src/main/java/com/example/android/NewsFeed.java
@@ -17,6 +17,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.w3c.dom.Text;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -25,6 +27,7 @@ public class NewsFeed extends Fragment {
     private MyAdapter adapter;
     private RecyclerView recyclerView;
     private List<RecyclerItem> listItems;
+    public static final String URL = "https://demonuts.com/Demonuts/JsonTest/Tennis/json_parsing.php";
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,21 +40,28 @@ public class NewsFeed extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        //СКАЧАТЬ С FIREBASE
         recyclerView = (RecyclerView) getActivity().findViewById(R.id.recyclerView);
+
+        String data = null;
+        try {
+            data = new URL(URL).toString();
+            System.out.println(data);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(data);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         listItems = new ArrayList<>();
+
         for(int i = 0; i < 10; i++){
             String url = "https://firebasestorage.googleapis.com/v0/b/image-259c9.appspot.com/o/knight.jpg?alt=media&token=178026e5-1d49-4716-8953-7ad1af3088df";
             RecyclerItem item = new RecyclerItem("Item " + (i + 1), "Description of item " + (i + 1), url);
             listItems.add(item);
         }
+
         adapter = new MyAdapter(listItems, getContext(), "main");
         recyclerView.setAdapter(adapter);
-
-
-
     }
 
 }
