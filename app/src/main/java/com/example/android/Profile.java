5be2784b659a2e2a7748c0c4cf5ac849fef99021
KGallyamov <mirakpjcppr@gmail.com>
2020-03-23 16:08:22 +0300

    design update

--- app/src/main/java/com/example/android/Profile.java
+++ app/src/main/java/com/example/android/Profile.java
@@ -1,5 +1,7 @@
 package com.example.android;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +15,7 @@ import androidx.fragment.app.Fragment;
 
 public class Profile extends Fragment {
     String role, login;
-    Button exit;
+    TextView exit;
     private TextView tv_login, password, tv_role;
 
     Profile(String role, String login){
@@ -35,7 +37,7 @@ public class Profile extends Fragment {
         super.onStart();
         tv_login = (TextView) getActivity().findViewById(R.id.login);
         tv_role  =(TextView) getActivity().findViewById(R.id.role);
-        exit = (Button) getActivity().findViewById(R.id.exit123456);
+        exit = (TextView) getActivity().findViewById(R.id.exit123456);
 
         tv_role.setText(role);
         tv_login.setText(login);
@@ -43,7 +45,23 @@ public class Profile extends Fragment {
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().finish();
+                AlertDialog.Builder ask = new AlertDialog.Builder(getContext(), R.style.MyAlertDialogStyle);
+                ask.setMessage("Are you sure you want to log out?").setCancelable(false)
+                .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getActivity().finish();
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alertDialog = ask.create();
+                alertDialog.setTitle("Log out");
+                alertDialog.show();
             }
         });
 
