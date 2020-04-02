ebc5b4e716ccd5cac2d6d6fbe88f2778fc2e5ef7
KGallyamov <mirakpjcppr@gmail.com>
2020-04-02 21:53:44 +0300

    checking if new login is already taken

--- app/src/main/java/com/example/android/PostPage.java
+++ app/src/main/java/com/example/android/PostPage.java
@@ -142,8 +142,7 @@ public class PostPage extends AppCompatActivity {
         }
         rating.put("zero", (float) 0);
 
-        autoLinkTextView.setAutoLinkText(to_tag + " https://ru.stackoverflow.com/questions/219888/%D0%9A%D0%B0%D0%BA-%D0%BE%D1%82%D0%BA%D1%80%D1%8B%D1%82%D1%8C-%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D1%83-%D0%B2-%D1%81%D1%82%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D1%82%D0%BD%D0%BE%D0%BC-%D0%B1%D1%80%D0%B0%D1%83%D0%B7%D0%B5%D1%80%D0%B5-%D0%90%D0%BD%D0%B4%D1%80%D0%BE%D0%B8%D0%B4");
-
+        autoLinkTextView.setAutoLinkText(to_tag);
         if(!role.equals("user")){
             ok.setVisibility(View.VISIBLE);
             refuse.setVisibility(View.VISIBLE);
